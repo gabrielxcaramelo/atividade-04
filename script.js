4b3aaef4a95228b08d6f794153e59698d3cc9646
gabrielxcaramelo <106408431+gabrielxcaramelo@users.noreply.github.com>
2022-10-27 17:28:00 -0300

    atividade da ultima aula

---
+++ script.js
@@ -0,0 +1,45 @@
+var signo = prompt ("Digite seu signo");
+
+switch (signo){
+
+    case  'aries':
+
+        console.log('de 21 de março a 20 de abril');
+
+    break;
+
+    case 'touro':
+
+        console.log('de 21 abril a 20 maio');
+
+    break;
+
+    case 'gemeos':
+
+        console.log(' de 21 maio a 20 junho');
+
+    break;
+
+    case 'cancer':
+
+        console.log('de 21 junho a 22 julho');
+
+    break;
+
+    case 'leao':
+
+        console.log('de 23 julho a 22 agosto');
+
+    break;
+
+    case'virgem':
+
+    console.log(' de 23 agosto a 22 setembro');
+    break;
+
+   default: 'caso não tenha signo ';
+   console.log ('signo não registrado');
+   break;
+
+
+}
